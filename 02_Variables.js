dec1c21eed06abc8ad269ad6bb08ca7648566c06
RJ-4803 <renujaan4803@gmail.com>
2025-12-22 17:20:57 +0530

    feat: add new file

---
+++ 02_Variables.js
@@ -0,0 +1,7 @@
+let bookName,bookAuthor,bookPublisher;
+
+bookName="The Motorcycle Diaries";
+bookAuthor="Ernesto Che Guevara";
+bookPublisher="Verso Books";
+
+console.log(bookName+"\n"+bookAuthor+"\n"+bookPublisher);
