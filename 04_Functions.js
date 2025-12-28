78d2a3d48a481ae0bd4c23d413c28b3a7fc0a57a
RJ-4803 <renujaan4803@gmail.com>
2025-12-28 22:53:04 +0530

    feat: add new file

---
+++ 04_Functions.js
@@ -0,0 +1,15 @@
+function samsung(){
+    console.log("This is a mobile")
+}
+
+function nikon(){
+    console.log("This is a dslr")
+}
+
+function msi(){
+    console.log("This is a laptop")
+}
+
+samsung()
+nikon()
+msi()
