9c090465f16c07e0b2d7c90afb071e27904e6fbc
RJ-4803 <renujaan4803@gmail.com>
2025-12-29 16:02:29 +0530

    feat: add new file

---
+++ 05_If_Conditions.js
@@ -0,0 +1,6 @@
+if(false){
+    console.log("Take the umbrella")
+}
+else{
+    console.log("Enjoy the sunny")
+}
---
+++ 05_If_ConditionsEx.js
@@ -0,0 +1,13 @@
+let r="green"
+
+if(r=="red"){
+    console.log("Stop")
+}
+
+else if(r=="orange"){
+    console.log("ready")
+}
+
+else{
+    console.log("go")
+}
