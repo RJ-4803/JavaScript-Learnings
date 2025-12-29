81edfc23e06514bca498039fdb439f6bd0254c3c
RJ-4803 <renujaan4803@gmail.com>
2025-12-29 15:36:31 +0530

    fix: modify to extent the codes

--- 04_FunctionsEx_01.js
+++ 04_FunctionsEx_01.js
@@ -1,7 +1,33 @@
-// function exercise
+//Without arguments without return value
+function display(){
+    console.log("Display:")
+}
+
+display()
+
+//With arguments without return value
 function add(a,b){
     console.log(a+b)
 
 }
 
 add(1,5)
+
+
+
+
+
+////Without arguments with return value
+function retrn(){
+    return "Hello"
+}
+
+console.log(retrn())
+
+//With arguments with return value
+function mul(c,d){
+    return c*d
+
+}
+
+console.log(mul(4,8))
